05d5e741411e7411f8cd81fd3a0b5a9adec1183c
Kazuya Takei <dev@attakei.net>
2026-09-14 01:15:26 +0900

    chore: initial commit
    - Copy project configuration files from my other repositories.
    - Run `nimble init` and `nimble setup` to start as Nim project.
    - Run `sphinx-quickstart` to generate documentation.
    - Generate all lock files.
    - Configure and pass all files.

---
+++ atskkserv.nimble
@@ -0,0 +1,15 @@
+# Package
+
+version       = "0.0.0"
+author        = "Kazuya Takei"
+description   = "attakei's SKK server"
+license       = "Apache-2.0"
+srcDir        = "src"
+binDir        = "dist"
+installExt    = @["nim"]
+bin           = @["atskkserv"]
+
+
+# Dependencies
+
+requires "nim >= 2.2.0"
---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+# Configuration file for the Sphinx documentation builder.
+# -- Project information
+project = 'atskkserv'
+copyright = '2026, Kazuya Takei'
+author = 'Kazuya Takei'
+release = '0.0.0'
+
+# -- General configuration
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+language = 'ja'
+
+# -- Options for HTML output
+html_theme = 'alabaster'
+html_static_path = ['_static']
