26e0b78557d8dac326cc7f6c1a8cf57ee5188f08
Kazuya Takei <dev@attakei.net>
2026-09-15 04:32:21 +0900

    ci: add test for multiple connection to server

--- tests/e2e/test_it.py
+++ tests/e2e/test_it.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
 
+import concurrent.futures as cf
+import socket
+import threading
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from socket import socket
+    from .conftest import Address
 
 
-def recv_all(sock: socket, bufsize: int = 256, idle_timeout: float = 0.5) -> bytes:
+def recv_all(sock: socket.socket, bufsize: int = 256, idle_timeout: float = 0.5) -> bytes:
     """ソケットからデータが届かなくなるまで受信し続ける。
 
     サーバー実装がバイト単位で分割送信することがあるため、
@@ -29,6 +32,32 @@ def recv_all(sock: socket, bufsize: int = 256, idle_timeout: float = 0.5) -> byt
     return ret
 
 
-def test_connect(skk_client: socket):
+def test_connect(skk_client: socket.socket):
     skk_client.send(b"HELLO")
     assert recv_all(skk_client) == b"HELLO"
+
+
+def test_multiple_connect(skk_server: Address):
+    """同時接続性のテスト。"""
+    texts = [
+        b"HELLO",
+        b"SKK",
+        b"WORLD",
+    ]
+    barrier = threading.Barrier(len(texts))
+
+    def worker(data: bytes):
+        barrier.wait()
+        with socket.create_connection(skk_server, timeout=1) as s:
+            s.send(data)
+            return recv_all(s)
+
+    with cf.ThreadPoolExecutor(len(texts)) as ex:
+        results = [
+            f.result()
+            for f in [
+                ex.submit(worker, t)
+                for t in texts
+            ]
+        ]
+        assert sorted(results) == sorted(texts)
